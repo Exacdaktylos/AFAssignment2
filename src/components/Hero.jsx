c72eb50b0a549c41f59c6af046e09b6c7c52b2d8
Yohan Wickramasinghe <103227132+Exacdaktylos@users.noreply.github.com>
2024-05-15 18:13:02 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,12 @@ const Hero = () => {
         >
           <h1
             className={
-              "text-white text-center text-[100px] md:text-[200px]  font-bold backdrop-blur-[1px]"
+              "text-white text-center text-[100px] md:text-[200px]  font-bold backdrop-blur-[1px]" style={{
+      fontFamily: 'Poppins'
+    }}
             }
           >
-            NASA EXPLORE<span className={"text-[#957ffd]"}>R</span>
+            NASA EXPLORE<span className={"text-[#005DFF]"}>R</span>
           </h1>
         </div>
       </div>
